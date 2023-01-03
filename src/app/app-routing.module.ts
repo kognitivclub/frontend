bd04e3267c76542c572beed155106335c1808ca0
pavansai <pavansaigontina123@gmail.com>
2023-01-03 12:49:59 +0530

    docs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { IndexComponent } from './index/index.component';
+const routes: Routes = [
+  {path : 'index', component : IndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
